febd9f20594d50db292f94faaf644545262a0bbe
Fernando Nakagawa <fernando.nakagawa@sesisenaipr.org.br>
2019-10-25 14:59:03 -0300

    Início

---
+++ app/src/main/java/com/example/senai/api_conexao_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.senai.api_conexao_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
