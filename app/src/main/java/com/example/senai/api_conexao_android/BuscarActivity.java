10f0e93fccde4c5c107be19acde2f2deb28c1458
Fernando Nakagawa <fernando.nakagawa@sesisenaipr.org.br>
2019-11-06 21:36:12 -0300

    Iniciando layout da busca.

---
+++ app/src/main/java/com/example/senai/api_conexao_android/BuscarActivity.java
@@ -0,0 +1,14 @@
+package com.example.senai.api_conexao_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar);
+    }
+}
